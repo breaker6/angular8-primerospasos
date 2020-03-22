9db1c8be027393f18577345c4b9c29121b07459d
Fernando Beltran <fernando.beltran.brandez@gmail.com>
2020-03-22 18:56:25 +0100

    Ultimos ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //Añadido FormsModule por que sin el no funcionará ngmodel
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CorreoComponent } from './Component/correo/correo.component';
 import { ListaCorreoComponent } from './Component/lista-correo/lista-correo.component';
+import { NuevoCorreoComponent } from './Component/nuevo-correo/nuevo-correo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CorreoComponent,
-    ListaCorreoComponent
+    ListaCorreoComponent,
+    NuevoCorreoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
